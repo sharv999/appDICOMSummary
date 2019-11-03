8725c86efeb0e5c43f65e6a1fd08d007e03b07c6
Steven Harvey <53710996@lan.sesahs.nsw.gov.au>
2019-11-03 13:22:11 +1100

    Revised code to v2

--- public/javascripts/extractData.js
+++ public/javascripts/extractData.js
@@ -2,140 +2,205 @@
 *
 * MODULE: 		extractData.js
 *
-* PROJECT: 		appDICOMSummary
+* PROJECT: 		appDICOMSummary2
 *
-* DESCRIPTION:	Parsing routines for index.html
+* DESCRIPTION:	Data extraction routines for index.html
 *
 ***********************************************************/
 
-    function dumpDataSet(dataSet)
-    {
-        $('span[data-dicom]').each(function(index, value)
-        {
-            var attr = $(value).attr('data-dicom');
-            var element = dataSet.elements[attr];
-            var text = "";
-            if(element !== undefined)
-            {
-                var str = dataSet.string(attr);
-                if(str !== undefined) {
-                    text = str;
-                }
-            }
-            $(value).text(text);
-        });
-
-        $('span[data-dicomUint]').each(function(index, value)
-        {
-            var attr = $(value).attr('data-dicomUint');
-            var element = dataSet.elements[attr];
-            var text = "";
-            if(element !== undefined)
-            {
-                if(element.length === 2)
-                {
-                    text += dataSet.uint16(attr);
-                }
-                else if(element.length === 4)
-                {
-                    text += dataSet.uint32(attr);
-                }
-            }
-
-            $(value).text(text);
-        });
-
-    }
-
-    // This function will read the file into memory and then start dumping it
-    function dumpFile(file)
-    {
-        $('#status').removeClass('alert-warning alert-success alert-danger').addClass('alert-info');
-        $('#warnings').empty();
-        document.getElementById('statusText').innerHTML = 'Status: Loading file, please wait..';
-
-        var reader = new FileReader();
-        reader.onload = function(file) {
-            var arrayBuffer = reader.result;
-
-            // Here we have the file data as an ArrayBuffer.  dicomParser requires as input a
-            // Uint8Array so we create that here
-            var byteArray = new Uint8Array(arrayBuffer);
-
-            var kb = byteArray.length / 1024;
-            var mb = kb / 1024;
-            var byteStr = mb > 1 ? mb.toFixed(3) + " MB" : kb.toFixed(0) + " KB";
-            document.getElementById('statusText').innerHTML = '<span class="glyphicon glyphicon-cog"></span>Status: Parsing ' + byteStr + ' bytes, please wait..';
-
-            // set a short timeout to do the parse so the DOM has time to update itself with the above message
-            setTimeout(function() {
-
-                var dataSet;
-                // Invoke the paresDicom function and get back a DataSet object with the contents
-                try {
-                    var start = new Date().getTime();
-
-                    dataSet = dicomParser.parseDicom(byteArray);
-                    // Here we call dumpDataSet to update the DOM with the contents of the dataSet
-                    dumpDataSet(dataSet);
-
-                    var end = new Date().getTime();
-                    var time = end - start;
-                    if(dataSet.warnings.length > 0)
-                    {
-                        $('#status').removeClass('alert-success alert-info alert-danger').addClass('alert-warning');
-                        $('#statusText').html('Status: Warnings encountered while parsing file (file of size '+ byteStr + ' parsed in ' + time + 'ms)');
-
-                        dataSet.warnings.forEach(function(warning) {
-                            $("#warnings").append('<li>' + warning +'</li>');
-                        });
-                    }
-                    else
-                    {
-                        var pixelData = dataSet.elements.x7fe00010;
-                        if(pixelData) {
-                            $('#status').removeClass('alert-warning alert-info alert-danger').addClass('alert-success');
-                            $('#statusText').html('Status: Ready (file of size '+ byteStr + ' parsed in ' + time + 'ms)');
-                        }
-                        else
-                        {
-                            $('#status').removeClass('alert-warning alert-info alert-danger').addClass('alert-success');
-                            $('#statusText').html('Status: Ready - no pixel data found (file of size ' + byteStr + ' parsed in ' + time + 'ms)');
-                        }
-                    }
-                }
-                catch(err)
-                {
-                    $('#status').removeClass('alert-success alert-info alert-warning').addClass('alert-danger');
-                    document.getElementById('statusText').innerHTML = 'Status: Error - ' + err + ' (file of size ' + byteStr + ' )';
-                }
-
-            }, 30);
-        };
-
-        reader.readAsArrayBuffer(file);
-    }
-
-
+ // Initialise cornerstone
+ cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
     // this function gets called once the user drops the file onto the div
+	
     function handleFileSelect(evt) {
         evt.stopPropagation();
         evt.preventDefault();
-
         // Get the FileList object that contains the list of files that were dropped
-        var files = evt.dataTransfer.files;
-
+        const files = evt.dataTransfer.files;
         // this UI is only built for a single file so just dump the first one
-        dumpFile(files[0]);
+        file = files[0];
+        const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+        loadAndViewImage(imageId);
     }
-
+	
     function handleDragOver(evt) {
         evt.stopPropagation();
         evt.preventDefault();
         evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
     }
-
     // Setup the dnd listeners.
-    var dropZone = document.getElementById('dropZone');
+    const dropZone = document.getElementById('dicomImage');
     dropZone.addEventListener('dragover', handleDragOver, false);
     dropZone.addEventListener('drop', handleFileSelect, false);
+    cornerstoneWADOImageLoader.configure({
+        beforeSend: function(xhr) {
+            // Add custom headers here (e.g. auth tokens)
+            //xhr.setRequestHeader('x-auth-token', 'my auth token');
+        },
+        useWebWorkers: true,
+    });
+    
+	// File loded success flag
+	let loaded = false;
+	
+	// Main
+    function loadAndViewImage(imageId) {
+        const element = document.getElementById('dicomImage');
+        const start = new Date().getTime();
+		
+		$('#status').removeClass('alert-warning alert-success alert-danger').addClass('alert-info');
+        $('#warnings').empty();
+        document.getElementById('statusText').innerHTML = 'Status: Loading file, please wait..';
+		
+        cornerstone.loadImage(imageId).then(function(image) {
+            //console.log(image);
+            const viewport = cornerstone.getDefaultViewportForImage(element, image);
+            //document.getElementById('toggleModalityLUT').checked = (viewport.modalityLUT !== undefined);
+            //document.getElementById('toggleVOILUT').checked = (viewport.voiLUT !== undefined);
+            cornerstone.displayImage(element, image, viewport);
+            if(loaded === false) {
+                cornerstoneTools.mouseInput.enable(element);
+                cornerstoneTools.mouseWheelInput.enable(element);
+                cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+                cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+                cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+                cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+                //cornerstoneTools.imageStats.enable(element);
+                loaded = true;			
+            }
+			
+			// Display data
+            function getTransferSyntax() {
+                const value = image.data.string('x00020010');
+                return value + ' [' + uids[value] + ']';
+            }
+            function getSopClass() {
+                const value = image.data.string('x00080016');
+                return value + ' [' + uids[value] + ']';
+            }
+            function getPixelRepresentation() {
+                const value = image.data.uint16('x00280103');
+                if(value === undefined) {
+                    return;
+                }
+                return value + (value === 0 ? ' (unsigned)' : ' (signed)');
+            }
+            function getPlanarConfiguration() {
+                const value = image.data.uint16('x00280006');
+                if(value === undefined) {
+                    return;
+                }
+                return value + (value === 0 ? ' (pixel)' : ' (plane)');
+            }
+			// Patient data
+			document.getElementById('patientName').textContent = image.data.string('x00100010');
+			document.getElementById('patientID').textContent = image.data.string('x00100020');
+			document.getElementById('patientDOB').textContent = image.data.string('x00100030');
+			document.getElementById('patientSex').textContent = image.data.string('x00100040');
+			// Study data
+			document.getElementById('studyDescription').textContent = image.data.string('x00081030');
+			document.getElementById('studyProtocol').textContent = image.data.string('x00181030');
+			document.getElementById('studyAccessionNo').textContent = image.data.string('x00080050');
+			document.getElementById('studyID').textContent = image.data.string('x00200010');
+			document.getElementById('studyDate').textContent = image.data.string('x00080020');
+			document.getElementById('studyTime').textContent = image.data.string('x00080030');
+			document.getElementById('institutionName').textContent = image.data.string('x00080080');
+			document.getElementById('studyUID').textContent = image.data.string('x0020000d');
+			// Series data
+			document.getElementById('seriesDescription').textContent = image.data.string('x0008103e');
+			document.getElementById('seriesNo').textContent = image.data.string('x00200011');
+			document.getElementById('seriesModality').textContent = image.data.string('x00080060');
+			document.getElementById('seriesManufacturer').textContent = image.data.string('x00080070');
+			document.getElementById('seriesModel').textContent = image.data.string('x00081090');
+			document.getElementById('seriesStationName').textContent = image.data.string('x00081010');
+			document.getElementById('seriesAETitle').textContent = image.data.string('x00020016');	
+			document.getElementById('seriesBodyPart').textContent = image.data.string('x00180015');
+			document.getElementById('acquisitionDate').textContent = image.data.string('x00080022');
+			document.getElementById('acquisitionTime').textContent = image.data.string('x00080032');
+			document.getElementById('seriesUID').textContent = image.data.string('x0020000e');
+			// Image data
+			document.getElementById('transferSyntax').textContent = getTransferSyntax();
+            document.getElementById('sopClass').textContent = getSopClass();
+            document.getElementById('samplesPerPixel').textContent = image.data.uint16('x00280002');
+            document.getElementById('photometricInterpretation').textContent = image.data.string('x00280004');
+            document.getElementById('numberOfFrames').textContent = image.data.string('x00280008');
+            document.getElementById('planarConfiguration').textContent = getPlanarConfiguration();
+            document.getElementById('rows').textContent = image.data.uint16('x00280010');
+            document.getElementById('columns').textContent = image.data.uint16('x00280011');
+            document.getElementById('pixelSpacing').textContent = image.data.string('x00280030');
+            document.getElementById('bitsAllocated').textContent = image.data.uint16('x00280100');
+            document.getElementById('bitsStored').textContent = image.data.uint16('x00280101');
+            document.getElementById('highBit').textContent = image.data.uint16('x00280102');
+            document.getElementById('pixelRepresentation').textContent = getPixelRepresentation();
+            document.getElementById('windowCenter').textContent = image.data.string('x00281050');
+            document.getElementById('windowWidth').textContent = image.data.string('x00281051');
+            document.getElementById('rescaleIntercept').textContent = image.data.string('x00281052');
+            document.getElementById('rescaleSlope').textContent = image.data.string('x00281053');
+            document.getElementById('basicOffsetTable').textContent = image.data.elements.x7fe00010 && image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '';
+            document.getElementById('fragments').textContent = image.data.elements.x7fe00010 && image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '';
+            document.getElementById('minStoredPixelValue').textContent = image.minPixelValue;
+            document.getElementById('maxStoredPixelValue').textContent = image.maxPixelValue;
+			document.getElementById('instanceUID').textContent = image.data.string('x00080018');
+            const end = new Date().getTime();
+            const time = end - start;
+            //document.getElementById('totalTime').textContent = time + "ms";
+            //document.getElementById('loadTime').textContent = image.loadTimeInMS + "ms";
+            //document.getElementById('decodeTime').textContent = image.decodeTimeInMS + "ms";
+			$('#status').removeClass('alert-warning alert-info alert-danger').addClass('alert-success');
+            $('#statusText').html('Status: File parsed in ' + time + ' ms');	
+        }, function(err) {
+			$('#status').removeClass('alert-success alert-info alert-warning').addClass('alert-danger');
+            document.getElementById('statusText').innerHTML = 'Status: Error - ' + err;
+            //alert(err);
+        });
+    }
+    //cornerstone.events.addEventListener('cornerstoneimageloadprogress', function(event) {
+    //    const eventData = event.detail;
+    //    const loadProgress = document.getElementById('loadProgress');
+    //    loadProgress.textContent = `Image Load Progress: ${eventData.percentComplete}%`;
+    //});
+	
+    // Display image
+	const element = document.getElementById('dicomImage');
+    cornerstone.enable(element);
+    document.getElementById('selectFile').addEventListener('change', function(e) {
+        // Add the file to the cornerstoneFileImageLoader and get unique
+        // number for that file
+        const file = e.target.files[0];
+        const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+        loadAndViewImage(imageId);
+    });
+	
+    //document.getElementById('toggleModalityLUT').addEventListener('click', function() {
+    //    const applyModalityLUT = document.getElementById('toggleModalityLUT').checked;
+    //    console.log('applyModalityLUT=', applyModalityLUT);
+    //    const image = cornerstone.getImage(element);
+    //    const viewport = cornerstone.getViewport(element);
+    //    if(applyModalityLUT) {
+    //        viewport.modalityLUT = image.modalityLUT;
+    //    } else {
+    //        viewport.modalityLUT = undefined;
+    //    }
+    //   cornerstone.setViewport(element, viewport);
+    //});
+	
+    //document.getElementById('toggleVOILUT').addEventListener('click', function() {
+    //    const applyVOILUT = document.getElementById('toggleVOILUT').checked;
+    //    console.log('applyVOILUT=', applyVOILUT);
+    //    const image = cornerstone.getImage(element);
+    //    const viewport = cornerstone.getViewport(element);
+    //    if(applyVOILUT) {
+    //       viewport.voiLUT = image.voiLUT;
+    //    } else {
+    //        viewport.voiLUT = undefined;
+    //    }
+    //    cornerstone.setViewport(element, viewport);
+    //});
+	
+    //document.getElementById('toggleCollapseInfo').addEventListener('click', function() {
+    //    if (document.getElementById('collapseInfo').style.display === 'none') {
+    //        document.getElementById('collapseInfo').style.display = 'block';
+    //    } else {
+    //        document.getElementById('collapseInfo').style.display = 'none';
+    //    }
+    //});
